f54c2cb48c5d44a4e60d76f052a91295738c8e95
Robert Liebhaber <liebhaber.robert@gmail.com>
2019-10-12 12:52:50 +0200

    Add author info and quetsion creation date in question item

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -4,15 +4,56 @@ namespace App\Http\Controllers;
 
 use App\Question;
 use Illuminate\Http\Request;
-
+use DateTime;
+use App\User;
 class QuestionsController extends Controller
 {
+    function dateDifference($date_1 , $date_2 , $differenceFormat = '%a' )
+    {
+        $datetime1 = date_create($date_1);
 
+        $datetime2 = date_create($date_2);
+
+        $interval = date_diff($datetime1, $datetime2);
+
+        return $interval->format($differenceFormat);
+
+    }
+    public function timeForAnEmail($dateTime1, $dateTime2){
+        return $this->dateDifference($dateTime1,$dateTime2)>5?'true':'false';
+    }
     public function index()
     {
-        $questions=Question::latest()->paginate(5);
 
-        return view('questions.index', compact('questions'));
+        $questions=Question::with('user')->latest()->paginate(5);
+//        //    $question = Question::find(1);
+//
+//          $now = new DateTime();
+////        $lastUpdate =  $question->updated_at;
+////        echo($this->timeForAnEmail($now->format('Y-m-d H:i:s'),$lastUpdate->format('Y-m-d H:i:s')));
+////        echo('difference between the 2 Datetimes: ' . $this->dateDifference($now->format('Y-m-d H:i:s'),$lastUpdate->format('Y-m-d H:i:s')));
+////        echo('<br/>' . $now->format('Y-m-d H:i:s'));
+////        echo('<br/>' . $question->updated_at->format('Y-m-d H:i:s'));
+//          $alle =[];
+//        foreach($questions as $quest){
+//            $lastUpdate = $quest->updated_at;
+//            $difference = $this->dateDifference($now->format('Y-m-d H:i:s'),$lastUpdate->format('Y-m-d H:i:s'));
+//            if($difference>1){
+//                 $alle = ('Email senden an ' . User::find($quest->user_id)->email);
+//            }
+//        }
+//        $question = $questions[0];
+//        $time = date_create($question->created_at);
+//        $min = date_format($now,'y');
+//        $hour = date_format($now,'h');
+//        //dd($min);
+//        //dd($hour);
+//       // dd($now);
+//        dd($alle);
+//        dd('Vorlaeuffiges Ende!');
+        return view('questions.index', compact('questions')) ;
+
+
     }
 
     public function create()
--- app/Question.php
+++ app/Question.php
@@ -11,6 +11,7 @@ class Question extends Model
         'title','body','slug'
     ];
 
+
         public function user()
         {
             return $this->belongsTo(User::class);
@@ -20,4 +21,13 @@ class Question extends Model
             $this->attributes['title']=$value;
             $this->attributes['slug']= Str::slug($value);
         }
+
+        public function getUrlAttribute(){
+            return route('questions.show',$this->id);
+        }
+
+        public function getCreatedDateAttribute(){
+            return $this->created_at->diffForHumans();
+        }
+
 }
