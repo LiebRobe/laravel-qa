a67cfedc05afa66368190ad42108a1ca80b6e9b9
LiebRobe <41720902+LiebRobe@users.noreply.github.com>
2019-10-17 12:20:40 +0200

    create new master_branch

--- database/migrations/2019_10_17_101147_create_favorites_table.php
+++ database/migrations/2019_10_17_101147_create_favorites_table.php
@@ -21,11 +21,7 @@ class CreateFavoritesTable extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
+
     public function down()
     {
         Schema::dropIfExists('favorites');
