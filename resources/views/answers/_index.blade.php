02c7bbeed3c976e627fa87154fa31f610771cb8b
LiebRobe <41720902+LiebRobe@users.noreply.github.com>
2019-10-17 11:47:32 +0200

    41 Accepting the answer as best answer

--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -20,16 +20,29 @@
                             <a title="This answer is not usefull" class="vote-down off">
                                 <i class="fa fa-caret-down fa-3x"></i>
                             </a>
-
-                            <a
-                                title="Mark this answer as best answer"
+                            @can('accept',$answer)
+                                <a
+                                    title="Mark this answer as best answer"
+                                    class="{{ $answer->status }} mt-5"
+                                    onclick="event.preventDefault(); document.getElementById('accept-answer-{{ $answer->id }}').submit();"
+                                    >
+                                    <i class="far fa-check-square fa-2x"></i>
+                                    <span class="favorites-count">123</span>
+                                </a>
+                                <form id="accept-answer-{{ $answer->id }}" action="{{ route('answer.accept',$answer->id) }}" method="POST" style="display:none;">
+                                    @csrf
+                                </form>
+                            @else
+                                @if($answer->is_best)
+                                <a
+                                title="The question owner accepted this answer as best answer"
                                 class="{{ $answer->status }} mt-5"
-                                onclick="event.preventDefault(); document.getElementById('accept-answer-{{ $answer->id }}').submit();"
                                 >
                                 <i class="far fa-check-square fa-2x"></i>
                                 <span class="favorites-count">123</span>
                             </a>
-                            <form id="accept-answer-{{ $answer->id }}" action="{{ route('answer.accept',$answer->id) }}" method="POST" style="display:none;">@csrf</form>
+                                @endif
+                            @endcan
                         </div>
 
                         <div class="media-body">
